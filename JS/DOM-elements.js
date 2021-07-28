9ec7250d10a8b85bb41cf8c5bd71fd724e976b9d
Stanislaw <s.szarapka10@gmail.com>
2021-07-28 16:10:20 +0200

    add visual effects

--- JS/DOM-elements.js
+++ JS/DOM-elements.js
@@ -1,11 +1,14 @@
-export class Elements {
+class Elements {
   constructor() {
     this.settingsBtn = document.querySelector('.header__settings')
     this.settingsWheelBtn = this.settingsBtn.querySelector('.fa-cog')
+    this.tabs = document.querySelector('.display__tabs')
     this.pomodoro = document.querySelector('.pomodoro')
     this.shortBreak = document.querySelector('.short-break')
+    this.line = document.querySelector('.display__tabs--line')
     this.startStop = document.querySelector('.display__start-stop')
     this.addTask = document.querySelector('.tasks__add')
     this.form = document.querySelector('.tasks__form')
   }
 }
+export const DOM = new Elements()
--- JS/design-fetures.js
+++ JS/design-fetures.js
@@ -1,3 +1,32 @@
 'use strict'
 
-import { Elements } from './DOM-elements.js'
+import { DOM } from './DOM-elements.js'
+
+const setTabLine = function (e, el = false) {
+    let element
+    el ? (element = el) : (element = e.target)
+
+    if (element.classList.contains('display__tabs')) return
+
+    const w = element.getBoundingClientRect().width
+    const l = element.getBoundingClientRect().left
+    const t =
+        element.getBoundingClientRect().top +
+        element.getBoundingClientRect().height
+
+    DOM.line.style.width = w + 'px'
+    DOM.line.style.left = l + 'px'
+    DOM.line.style.top = t + 'px'
+}
+
+DOM.startStop.addEventListener(
+    'mousedown',
+    () => (DOM.startStop.style.boxShadow = 'none')
+)
+DOM.startStop.addEventListener(
+    'mouseup',
+    () => (DOM.startStop.style.boxShadow = '2px 4px 0px #b53c2f')
+)
+
+setTabLine(null, DOM.pomodoro)
+DOM.tabs.addEventListener('click', setTabLine)

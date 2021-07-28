361b227f5677bb16a4136b2484d26d6c0e664829
Stanislaw <s.szarapka10@gmail.com>
2021-07-28 11:52:49 +0200

    create dom elements class

---
+++ JS/DOM-elements.js
@@ -0,0 +1,11 @@
+class Elements {
+  constructor() {
+    this.settingsBtn = document.querySelector('.header__settings')
+    this.settingsWheelBtn = this.settingsBtn.querySelector('.fa-cog')
+    this.pomodoro = document.querySelector('.pomodoro')
+    this.shortBreak = document.querySelector('.short-break')
+    this.startStop = document.querySelector('.display__start-stop')
+    this.addTask = document.querySelector('.tasks__add')
+    this.form = document.querySelector('.tasks__form')
+  }
+}
